69cc076466db3e88f33c5de01be48e04ecdae24b
Edwin Cardenas <eacardenase@unal.edu.co>
2023-03-23 11:40:31 -0500

    Initial setup

--- LayoutMarginsLab/ViewController.swift
+++ LayoutMarginsLab/ViewController.swift
@@ -11,9 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
     }
-
-
 }
 
+extension ViewController {
+    func setupViews() {
+        let redView = UIView()
+        
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.backgroundColor = .red
+        
+        view.addSubview(redView)
+        
+        NSLayoutConstraint.activate([
+            redView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            redView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            redView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            redView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
+        ])
+    }
+}
